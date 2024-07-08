969a5559260432155170961a7f3ecfa217abe441
Kylan-Thurairajah <115436152+Kylan-Thurairajah@users.noreply.github.com>
2024-07-07 23:21:14 -0400

    deploy2

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,44 +1,33 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
-import "./App.css"
+import "./App.css" // Ensure this is imported
 
 function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
       <Container>
-        <Navbar.Brand href="/" className="mr-4 oswald-text">
+        <Navbar.Brand href="#home" className="mr-auto">
           GOLF CLUB 176
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="navbar-nav">
-            <Nav.Link href="/" className="nav-link-custom mx-2 oswald-text">
+            <Nav.Link as={Link} to="/" className="nav-link-custom">
               HOME
             </Nav.Link>
-            <Nav.Link
-              href="/About"
-              className="nav-link-custom mx-2 oswald-text"
-            >
+            <Nav.Link as={Link} to="/about" className="nav-link-custom">
               ABOUT
             </Nav.Link>
-            <Nav.Link
-              href="/Holes"
-              className="nav-link-custom mx-2 oswald-text"
-            >
+            <Nav.Link as={Link} to="/holes" className="nav-link-custom">
               HOLES
             </Nav.Link>
-            <Nav.Link
-              href="/Services"
-              className="nav-link-custom mx-2 oswald-text"
-            >
+            <Nav.Link as={Link} to="/services" className="nav-link-custom">
               SERVICES
             </Nav.Link>
-            <Nav.Link
-              href="/Membership"
-              className="nav-link-custom mx-2 oswald-text"
-            >
+            <Nav.Link as={Link} to="/membership" className="nav-link-custom">
               MEMBERSHIP
             </Nav.Link>
           </Nav>
